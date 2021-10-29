ba813d6151bd4886b80b3daeb60b71b1c2714eb4
eXpire163 <github@mms2k.de>
2021-10-29 12:44:11 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,208 @@
+let width = 700;
+let height = 400;
+let rotation = 0;
+let flash = true;
+
+leftEdge = -(width / 2) + 20
+rightEdge = (width / 2) - 20
+bottomPos = -height / 2
+topPos = height / 2
+
+function setup() {
+    createCanvas(width, height);
+
+    setInterval(cicleModes, 5000);
+}
+
+
+function draw() {
+    translate(width / 2, height / 2)
+    background(00);
+
+    runMode(99)
+}
+
+
+currentMode = 0
+totalModes = 4
+function cicleModes() {
+    currentMode = (currentMode + 1) % totalModes
+
+}
+
+beamRotation = 0
+beamRotation1 = 0
+beamRotation2 = 0
+function runMode(manualModeSelect = -1) {
+
+    selectedMode = (manualModeSelect>=0) ? manualModeSelect : currentMode
+
+    switch (selectedMode) {
+        case 0:
+            modeScannerHorizontal()
+            break;
+        case 1:
+            modeFadingCircle();
+            break;
+        case 2:
+            flash3Beams()
+            break;
+        case 3:
+            beamRotation = modeBeams(14, color(255, 0, 0), beamRotation, true, 120, true)
+            break;
+        case 4:
+            beamRotation1 = modeBeams(9, color(0, 0, 255), beamRotation1, true, 120)
+            beamRotation2 = modeBeams(7, color(0, 255, 0), beamRotation2, false, 80)
+            break;
+        case 5:
+
+            testPingPont()
+            break;
+        case 99:
+            modeScannerHorizontal()
+            modeFadingCircle();
+            flash3Beams()
+            beamRotation = modeBeams(14, color(255, 0, 0), beamRotation, true, 120, true)
+            beamRotation1 = modeBeams(9, color(0, 0, 255), beamRotation1, true, 120)
+            beamRotation2 = modeBeams(7, color(0, 255, 0), beamRotation2, false, 80)
+            testPingPont()
+            break;
+
+        default:
+
+            break;
+    }
+}
+
+
+// ############  Fading Circle #########
+rotationCounter = 0
+rotationSpeed = 0.02
+hueC = 0
+function modeFadingCircle() {
+
+    hueC++
+    if (hueC > 255) {
+        hueC = 0
+    }
+    v = polarToCartesian(height / 3, rotationCounter)
+
+    noFill()
+    colorMode(HSB);
+    stroke(hueC, 255, 255)
+    strokeWeight(5)
+    ellipse(v.x * 1.7, v.y, 140, 110);
+    ellipse(-v.x * 1.7, -v.y, 140, 110);
+    colorMode(RGB)
+    rotationCounter += rotationSpeed
+
+}
+
+
+
+// ############  Flashing Beams #########
+flashBeamRotation = 0
+function flash3Beams() {
+    if (flash) {
+      flashBeamRotation = modeBeams(3, color(255, 255, 255), flashBeamRotation, false)
+    }
+    flash = !flash
+}
+
+
+function modeBeams(number, c, currentRotation, direction = false, diameter = 100, changeHight) {
+
+    currentRotation = direction ? currentRotation + 0.05 : currentRotation - 0.05
+    pos = polarToCartesian(diameter, currentRotation)
+
+    for (i = 0; i < number; i++) {
+        pos = polarToCartesian(diameter, currentRotation + (i / number * 2 * Math.PI))
+        if(changeHight){
+
+            drawDot(pos.x, pos.y * sin(currentRotation), c)
+        }
+        else{
+            drawDot(pos.x, pos.y, c)
+        }
+    }
+    return currentRotation
+
+}
+
+
+function polarToCartesian(r, theta) {
+
+    // Convert polar to cartesian
+    let x = r * cos(theta);
+    let y = r * sin(theta);
+    return createVector(x, y)
+}
+
+
+
+function drawDot(x, y, c) {
+    strokeWeight(10);
+    stroke(c)
+    point(x, y);
+}
+
+
+// ############  Scanner #########
+function modeScannerHorizontal() {
+    movingBarLeftRight(true)
+}
+
+
+scannerCounter = 0
+scannerSpeed = 1
+scannerSteps = 200
+scannerWidth = 10
+
+
+function movingBarLeftRight(singleDouble) {
+
+
+    vStart = createVector(leftEdge, bottomPos)
+    vEnd = createVector(rightEdge, bottomPos)
+
+    scannerCounter += scannerSpeed
+    scannerCounter = scannerCounter % scannerSteps
+
+    barPos = pingPong(vStart, vEnd, scannerSteps, scannerCounter)
+    drawBarGreen(barPos.x, barPos.y, scannerWidth, height)
+    if (singleDouble) {
+        drawBarGreen(-barPos.x, barPos.y, scannerWidth, height)
+    }
+}
+
+function drawBarGreen(x, y, w, h) {
+    drawBar(x, y, w, h, 0, 255, 0)
+}
+
+function drawBar(x, y, w, h, r, g, b) {
+    fill(r, g, b);
+    noStroke();
+    rect(x, y, w, h);
+}
+
+//lerp including reverse
+function pingPong(vStart, vEnd, steps, currrentStep) {
+
+    if (currrentStep < steps / 2) {
+        delta = float(currrentStep) / (steps / 2)
+        return p5.Vector.lerp(vStart, vEnd, delta)
+    }
+    delta = float(currrentStep - (steps / 2)) / (steps / 2)
+    return p5.Vector.lerp(vEnd, vStart, delta)
+
+
+}
+tpp = 0.0
+function testPingPont() {
+    tpp += 0.1
+    left = createVector(-width / 2 + 10, 0)
+    right = createVector(width / 2 - 10, 0)
+    v = pingPong(left, right, 50, tpp)
+    drawDot(v.x, v.y, color(255,0,255))
+    tpp = tpp % 50
+}
